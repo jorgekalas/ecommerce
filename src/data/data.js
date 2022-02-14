416de8b34ad4502677644c01597035c166b034b5
jorgekalas <jorgekalas@gmail.com>
2022-02-14 20:26:38 -0300

    reemplazando archivos locales por urls en imágenes

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,9 @@
-import samsungS21 from '../assets/samsungS21.jpg'
-import motorolaG20 from '../assets/motorolaG20.jpg'
-import samsungA52 from '../assets/samsungA52.jpg'
-
-
 export const Productos = [
 {
     id:1,
     title: "Samsung S21",
     price: 120000,
-    picture: samsungS21
+    picture: "https://medias.musimundo.com/medias/00320175-140895-140895-01-140895-01.jpg-size515?context=bWFzdGVyfGltYWdlc3w1MzYyNXxpbWFnZS9qcGVnfGhiZC9oMGEvMTAzNDA5Mjk1Njg3OTgvMDAzMjAxNzUtMTQwODk1LTE0MDg5NV8wMS0xNDA4OTVfMDEuanBnX3NpemU1MTV8ODc1Mzc2ZGUwMWE1YzUzYjRiNmEwOTI0MTk4ZWE1NWQ4ZTkwYjQwOTcwYzkxMDMwZmY2ZTI0NmQ0NjM0NTU3MQ"
 
 },
 
@@ -16,7 +11,7 @@ export const Productos = [
     id:2,
     title: "Motorola G20",
     price: 30000,
-    picture: motorolaG20
+    picture: "https://medias.musimundo.com/medias/00448076-144398-144398-01-144398-01.jpg-size515?context=bWFzdGVyfGltYWdlc3w2MTQ4NXxpbWFnZS9qcGVnfGhjNy9oNGYvMTAzMTU5NjIxODc4MDYvMDA0NDgwNzYtMTQ0Mzk4LTE0NDM5OF8wMS0xNDQzOThfMDEuanBnX3NpemU1MTV8MTQzZDBlODk5NmJlYTViNjk0OTkzMzhhN2IyYjg4MzIyOTBlMThjZDI1MTYwM2ZmNjU0NmViOGFmYmZmZmI0MA"
 
 },
 
@@ -24,7 +19,7 @@ export const Productos = [
     id:3,
     title: "Samsung A52",
     price: 60000,
-    picture: samsungA52
+    picture: "https://medias.musimundo.com/medias/00325002-142196-142196-01-142196-01.jpg-size515?context=bWFzdGVyfGltYWdlc3w1NjUxOXxpbWFnZS9qcGVnfGgzNS9oZDYvMTAzMjMwMjQzMTQzOTgvMDAzMjUwMDItMTQyMTk2LTE0MjE5Nl8wMS0xNDIxOTZfMDEuanBnX3NpemU1MTV8ZDBkNTkwODkxZjlhZGMzZTFiMTYyNmJhYzY0ZjcxZWNhN2IzZTRmNTRmNWQ1OTZjMTAwYjNiMDlmZjc3NDUzNQ"
 
 },
 
